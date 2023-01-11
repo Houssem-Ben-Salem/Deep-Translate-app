20ea75513635426bac9e3b973ee15d5d0329dbe9
Houssem-Ben-Salem <houssem.bensalem@supcom.tn>
2023-01-11 10:41:01 +0100

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,78 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:texttospeachapp/Speachtotexttranslate.dart';
+import 'package:texttospeachapp/ml.dart';
+import 'package:texttospeachapp/texttranslate.dart';
+import 'package:texttospeachapp/utils/Colors.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomePage> {
+  var initialindex = 1;
+  var currentindex = 1;
+  var screens = [
+    const Imagetotext(),
+    const TextTranslate(),
+    const SpeachToTextTranslate()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      appBar: AppBar(
+        backgroundColor: colorsUsed.bottomcolor,
+        title: Center(
+            child: Text(
+          "Deep Translate",
+          style: TextStyle(color: colorsUsed.textcolor),
+        )),
+      ),
+      body: screens[currentindex],
+      bottomNavigationBar: CurvedNavigationBar(
+        color: colorsUsed.bottomcolor,
+        index: initialindex,
+        height: 50,
+        backgroundColor: Colors.transparent,
+        items: iconUsed.items,
+        onTap: (index) {
+          setState(() {
+            currentindex = index;
+          });
+
+          if (index == 0) {
+          } else if (index == 1) {
+            Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) =>
+                    TextTranslate(),
+                transitionDuration: Duration(seconds: 0),
+              ),
+            );
+            // Navigator.pushReplacement(context,
+            //     MaterialPageRoute(builder: (context) => const TextTranslate()));
+          } else if (index == 2) {
+            Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) =>
+                    SpeachToTextTranslate(),
+                transitionDuration: Duration(seconds: 0),
+              ),
+            );
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const SpeachToTextTranslate()));
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:texttospeachapp/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+      title: 'Text To Speech',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+      
+    );
+    
+  }
+}
